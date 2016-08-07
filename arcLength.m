e20dd1364469b7e43e0c26cface801f34da73da5
Patrik Karlström <jp.karlstrom@gmail.com>
2016-08-07 18:42:32 +0200

    Add documentation

--- arcLength.m
+++ arcLength.m
@@ -1,25 +1,8 @@
 function [arc_length] = arcLength(a)
-	%Solve the båglängd
+	% Calculate the length of the arc
     global q
  	y_prime = @(x) -0.3 * sin(sqrt(q)*x) * sqrt(q);
     y_arc = @(x) sqrt(1+y_prime(x).^2);
     
-    % arc_length = (a-0)*(y_arc(a)+y_arc(0))/2
-    
-    %h = 0.1;
-    %k = 1;
-    %N = a/h;
-    
-    %x_1 = (k-1)*h;
-    %x_2 = h;
-    %sum = 0;
-    
-    %for k = 1:h:(N+1)
-    %    sum = sum + y_arc(q, x_1) + y_arc(q, x_2);
-    %    x_1 = x_2;
-    %    x_2 = (k-1)*h;
-    %end
-    
-    %arc_length = sum * h/2;
  	arc_length = integral(y_arc, 0, a);
  end
--- hatarmittliv.m
+++ hatarmittliv.m
@@ -6,19 +6,26 @@ format long
 %sekantmetod
 x_2 = 0;
 flag=0;
+
+% For a given value for q
 for q= 16:0.001:18
     steps = 0;
+    
+    % Find an x value right before y = 0 and one right after
     [x_0,x_1] = guess_a();
     
+    % Using secant method try to interpolate the exact value where
+    % graph intersects the x axis
     while steps < 5
         steps = steps + 1;
         length = arcLength(x_0);
+        
+        % If we find a length that is sufficiently close, break
         if abs(length-0.5) < 1e-5
             a = x_0;
             flag=1;
             break
         end
-    
         x_2 = x_1 - ( x_1 - x_0 ) ./ ( y(x_1) - y(x_0) ) * y(x_1);  
 
         x_0 = x_1;
@@ -29,6 +36,8 @@ for q= 16:0.001:18
         break
     end
 end
+
+% Print values and draw plot
 q
 a
 length
